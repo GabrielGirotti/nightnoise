005bb659ad42453aeaedb5f578ec3cb4deb49121
Gabriel <girottigabriel@gmail.com>
2025-09-05 11:14:23 +0200

    flyer

---
+++ src/components/EventFlyerModal.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+interface EventFlyerModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  flyerSrc: string;
+  altText: string;
+}
+
+const EventFlyerModal: React.FC<EventFlyerModalProps> = ({
+  isOpen,
+  onClose,
+  flyerSrc,
+  altText,
+}) => {
+  if (!isOpen) return null;
+
+  return (
+    <div
+      className="fixed inset-0 z-50 flex items-center justify-center bg-black/70 p-4"
+      onClick={onClose}
+    >
+      {/* Contenido del modal */}
+      <div
+        className="relative shadow-lg  overflow-hidden "
+        onClick={(e) => e.stopPropagation()}
+      >
+        {/* Botón cerrar */}
+        <button
+          onClick={onClose}
+          className="absolute top-5 right-5 text-gray-200 hover:text-yellow-400 text-xl font-bold"
+        >
+          ✕
+        </button>
+
+        {/* Imagen del flyer */}
+        <img
+          src={flyerSrc}
+          alt={altText}
+          className="w-[100dvw] min-[563px]:max-h-[100dvh] min-[563px]:w-auto object-cover "
+        />
+      </div>
+    </div>
+  );
+};
+
+export default EventFlyerModal;
--- src/components/HeroBanner.tsx
+++ src/components/HeroBanner.tsx
@@ -1,121 +1,157 @@
+import { useState } from "react";
 import n2 from "../img/n2.png";
 import JendrixMap from "./JendrixMap";
 import RevueltaMap from "./RevueltaMap";
+import EventFlyerModal from "./EventFlyerModal";
 
 export default function HeroBanner() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [flyerData, setFlyerData] = useState<{ src: string; alt: string }>({
+    src: "",
+    alt: "",
+  });
+
+  const handleOpenModal = (src: string, alt: string) => {
+    setFlyerData({ src, alt });
+    setIsModalOpen(true);
+  };
+
   return (
-    <section
-      id="eventos"
-      className="bg-transparent relative text-white py-10 px-6 md:px-12 flex flex-col justify-center items-center"
-    >
-      <img
-        src={n2}
-        alt=""
-        className="absolute left-0 top-96 opacity-0 -z-30 md:opacity-10 w-[500px] max-w-[80dvw]"
-      />
-
-      <article
-        aria-labelledby="evento-viernes-title"
-        className="max-w-7xl mx-auto flex flex-col items-center justify-center gap-8 pb-5 transition-all duration-1000"
+    <>
+      <section
+        id="eventos"
+        className="bg-transparent relative text-white py-10 px-6 md:px-12 flex flex-col justify-center items-center"
       >
-        <header className="text-center space-y-3 flex flex-col justify-center">
-          <time
-            dateTime="2025-10-24T23:00"
-            className="text-xl text-gray-200 max-w-md mx-auto font-fty"
-          >
-            Viernes 24 de octubre
-          </time>
+        <img
+          src={n2}
+          alt=""
+          className="absolute left-0 top-96 opacity-0 -z-30 md:opacity-10 w-[500px] max-w-[80dvw]"
+        />
 
-          <h2
-            id="evento-viernes-title"
-            className="underline-effect-title md:text-4xl text-3xl"
-          >
-            Presentación del evento
-          </h2>
-
-          <p className="md:text-lg text-base text-gray-200 max-w-md mx-auto">
-            Pinchando <strong>K-NALLA SELEKTOR</strong> (Psychobilly,
-            Rockabilly, Surf, Garage)
-          </p>
-        </header>
-
-        <JendrixMap />
-
-        <div className="flex justify-center gap-4">
-          <header className="text-amber-950 font-bold p-4 py-2 bg-yellow-400 hover:bg-[#260617] hover:text-white rounded text-sm transition duration-300 text-center">
-            <time dateTime="2025-10-24T23:00">
-              Viernes 24 de octubre - 23hs
+        <article
+          aria-labelledby="evento-viernes-title"
+          className="max-w-7xl mx-auto flex flex-col items-center justify-center gap-8 pb-5 transition-all duration-1000"
+        >
+          <header className="text-center space-y-3 flex flex-col justify-center">
+            <time
+              dateTime="2025-10-24T23:00"
+              className="text-xl text-gray-200 max-w-md mx-auto font-fty"
+            >
+              Viernes 24 de octubre
             </time>
-            <p className="uppercase font-black text-base">Jendrix Bar</p>
+
+            <h2
+              id="evento-viernes-title"
+              className="underline-effect-title md:text-4xl text-3xl"
+            >
+              Presentación del evento
+            </h2>
+
+            <p className="md:text-lg text-base text-gray-200 max-w-md mx-auto">
+              Pinchando <strong>K-NALLA SELEKTOR</strong> (Psychobilly,
+              Rockabilly, Surf, Garage)
+            </p>
           </header>
-        </div>
-      </article>
 
-      <div className=" h-1 bg-yellow-600 opacity-15 z-40 md:max-w-[40dvw] mx-auto my-8"></div>
+          <JendrixMap />
 
-      <article
-        aria-labelledby="evento-sabado-title"
-        className="max-w-7xl mx-auto flex flex-col items-center gap-8 pt-5 transition-all duration-1000"
-      >
-        <header className="text-center space-y-3 flex flex-col justify-center">
-          <time
-            dateTime="2025-11-08T20:00"
-            className="text-xl text-gray-200 max-w-md mx-auto font-fty"
-          >
-            Sábado 8 de noviembre
-          </time>
-
-          <h2
-            id="evento-sabado-title"
-            className="underline-effect-title text-5xl"
-          >
-            Nightnoise Fest
-          </h2>
-        </header>
-
-        <section className="flex flex-col justify-center text-center space-y-2">
-          <p className="md:text-lg text-base text-gray-200 max-w-md mx-auto">
-            En vivo:
-          </p>
-          <ul className="flex flex-col justify-center items-center list-none list-inside font-bold font-heavy  tracking-wide text-4xl text-gray-200 max-w-md mx-auto">
-            <li>13 Bats</li>
-            <li>Donkey Kick</li>
-            <li>Los Turanga</li>
-            <li>La Llama</li>
-          </ul>
-          <div className=" flex flex-col md:flex-row md:gap-10">
-            <article>
-              <p className="text-lg  text-gray-200 max-w-md mx-auto pt-4">
-                DJs:
-              </p>
-              <ul className="text-lg  font-bold text-gray-200 max-w-md mx-auto">
-                <li>Jess VZ</li>
-                <li>Acho! El Kadhe</li>
-              </ul>
-            </article>
-            <article>
-              <p className="text-lg  text-gray-200 max-w-md mx-auto pt-4">
-                Stands:
-              </p>
-              <ul className="text-lg  font-bold text-gray-200 max-w-md mx-auto pb-2">
-                <li>Rebel Yell</li>
-                <li>Pipus Records</li>
-              </ul>
-            </article>
+          <div className="flex justify-center gap-4">
+            <header
+              onClick={() =>
+                handleOpenModal("/flyers/jendrix.jpg", "Flyer Jendrix Bar")
+              }
+              className="cursor-pointer text-amber-950 font-bold p-4 py-2 bg-yellow-400 hover:bg-[#260617] hover:text-white rounded text-sm transition duration-300 text-center"
+            >
+              <time dateTime="2025-10-24T23:00">
+                Viernes 24 de octubre - 23hs
+              </time>
+              <p className="uppercase font-black text-base">Jendrix Bar</p>
+              <p className=" text-sm">Ver cartel</p>
+            </header>
           </div>
-        </section>
+        </article>
 
-        <RevueltaMap />
+        <div className=" h-1 bg-yellow-600 opacity-15 z-40 md:max-w-[40dvw] mx-auto my-8"></div>
 
-        <div className="flex justify-center gap-4">
-          <header className="text-amber-950 font-bold p-4 py-2 bg-yellow-400 hover:bg-[#260617] hover:text-white rounded text-sm transition duration-300 text-center">
-            <time dateTime="2025-11-08T20:00">
-              Sábado 8 de noviembre - 20hs
+        <article
+          aria-labelledby="evento-sabado-title"
+          className="max-w-7xl mx-auto flex flex-col items-center gap-8 pt-5 transition-all duration-1000"
+        >
+          <header className="text-center space-y-3 flex flex-col justify-center">
+            <time
+              dateTime="2025-11-08T20:00"
+              className="text-xl text-gray-200 max-w-md mx-auto font-fty"
+            >
+              Sábado 8 de noviembre
             </time>
-            <p className="uppercase font-black text-base">Sala La Revuelta</p>
+
+            <h2
+              id="evento-sabado-title"
+              className="underline-effect-title text-5xl"
+            >
+              Nightnoise Fest
+            </h2>
           </header>
-        </div>
-      </article>
-    </section>
+
+          <section className="flex flex-col justify-center text-center space-y-2">
+            <p className="md:text-lg text-base text-gray-200 max-w-md mx-auto">
+              En vivo:
+            </p>
+            <ul className="flex flex-col justify-center items-center list-none list-inside font-bold font-heavy  tracking-wide text-4xl text-gray-200 max-w-md mx-auto">
+              <li>13 Bats</li>
+              <li>Donkey Kick</li>
+              <li>Los Turanga</li>
+              <li>La Llama</li>
+            </ul>
+            <div className=" flex flex-col md:flex-row md:gap-10">
+              <article>
+                <p className="text-lg  text-gray-200 max-w-md mx-auto pt-4">
+                  DJs:
+                </p>
+                <ul className="text-lg  font-bold text-gray-200 max-w-md mx-auto">
+                  <li>Jess VZ</li>
+                  <li>Acho! El Kadhe</li>
+                </ul>
+              </article>
+              <article>
+                <p className="text-lg  text-gray-200 max-w-md mx-auto pt-4">
+                  Stands:
+                </p>
+                <ul className="text-lg  font-bold text-gray-200 max-w-md mx-auto pb-2">
+                  <li>Rebel Yell</li>
+                  <li>Pipus Records</li>
+                </ul>
+              </article>
+            </div>
+          </section>
+
+          <RevueltaMap />
+
+          <div className="flex justify-center gap-4">
+            <header
+              onClick={() =>
+                handleOpenModal(
+                  "/flyers/revuelta.jpg",
+                  "Flyer Sala La Revuelta"
+                )
+              }
+              className="cursor-pointer text-amber-950 font-bold p-4 py-2 bg-yellow-400 hover:bg-[#260617] hover:text-white rounded text-sm transition duration-300 text-center"
+            >
+              <time dateTime="2025-11-08T20:00">
+                Sábado 8 de noviembre - 20hs
+              </time>
+              <p className="uppercase font-black text-base">Sala La Revuelta</p>
+              <p className=" text-sm">Ver cartel</p>
+            </header>
+          </div>
+        </article>
+      </section>
+      <EventFlyerModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        flyerSrc={flyerData.src}
+        altText={flyerData.alt}
+      />
+    </>
   );
 }
